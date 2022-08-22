3a5c2af5eef52c87f37519999e2f767a695cdf30
John Johnson Mbeba <john.mbeba@student.moringaschool.com>
2022-08-22 15:46:29 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const init = () => {
-  
+  const form = document.querySelector("form");
+  console.log(form);
+  form.addEventListener("submit", (e)=> {
+    e.preventDefault();
+    const input = document.getElementById("searchByID");
+
+    fetch(`http://localhost:3000/movies/${input.value}`).then(res => res.json()).then(data => {
+        const title = document.querySelector("#movieDetails h4");
+        const summary = document.querySelector("#movieDetails p");
+
+        title.textContent = data.title;
+        summary.textContent = data.summary;
+    });
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
